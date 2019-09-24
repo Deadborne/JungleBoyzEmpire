00bb42b4d937c9c2c868e1b4befde78f1888202b
ha-andrew <34655105+ha-andrew@users.noreply.github.com>
2019-09-24 16:27:45 -0400

    Added initial project

--- EightMinuteEmpire/EightMinuteEmpire.cpp
+++ EightMinuteEmpire/EightMinuteEmpire.cpp
@@ -2,10 +2,18 @@
 //
 
 #include "stdafx.h"
-
+#include <string>
+#include<iostream>
+using namespace std;
 
 int main()
 {
+	cout << "Hello World! What is your name? \n";
+	string name;
+	getline(cin, name);
+	cout << "Hello " << name << "!\n";
+	cout << "Please press enter to close the console :D";
+	getline(cin, name);
     return 0;
 }
 
