4d88842043cc9a1f3cbc3fc6ee2c1bcadb466c23
George Mavroeidis <gdmavroeidis@hotmail.com>
2019-10-03 03:24:47 -0400

    added arguments

--- Deck/Deck/Card.h
+++ Deck/Deck/Card.h
@@ -3,17 +3,21 @@
 
 class Card {
 	private:
-		bool isWildcard;
-		std::string material;
+		std::string good;
+		int amount;
 		std::string action1;
 		std::string action2;
+		std::string operate;
 
 	public:
 		Card();
-		Card(std::string mat, std::string act1, std::string act2);
+		Card(std::string gd, int num, std::string act1, std::string act2, std::string op);
+		Card(const Card &c);
 		~Card();
-		std::string getMaterial() const;
+		std::string getGood() const;
+		int getGoodAmount() const;
 		std::string getAction1() const;
 		std::string getAction2() const;
+		std::string getOperator() const;
 
 };
