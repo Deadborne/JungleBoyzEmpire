e8100dc9bf71b6e1c5114cb2cb48f8f10649b09b
George Mavroeidis <gdmavroeidis@hotmail.com>
2019-10-03 03:24:17 -0400

    additional arguments

--- Deck/Deck/Card.cpp
+++ Deck/Deck/Card.cpp
@@ -3,25 +3,40 @@
 using std::cout;
 using std::endl;
 
-Card::Card() : {
+Card::Card() {
+	cout << "Card Default Constructed" << endl;
+}
+
+Card::Card(std::string gd, int am, std::string act1, std::string act2, std::string op) :
+	good(gd),
+	amount(am),
+	action1(act1),
+	action2(act2),
+	operate(op)
+{
 	cout << "Card Constructed" << endl;
 }
 
-Card::Card(std::string mat, std::string act1, std::string act2) :
-	material(mat),
-	action1(act1),
-	action2(act2)
-
+Card::Card(const Card &c) :
+	good(c.good),
+	amount(c.amount),
+	action1(c.action1),
+	action2(c.action2),
+	operate(c.operate)
 {
-	cout << "Card Constructed" << endl;
+	cout << "Card Constructed and Copied" << endl;
 }
 
 Card::~Card() {
 	cout << "Card Destructed" << endl;
 }
 
-inline std::string Card::getMaterial() const {
-	return material;
+inline std::string Card::getGood() const {
+	return good;
+}
+
+inline int Card::getGoodAmount() const {
+	return amount;
 }
 
 inline std::string Card::getAction1() const {
@@ -30,4 +45,8 @@ inline std::string Card::getAction1() const {
 
 inline std::string Card::getAction2() const {
 	return action2;
+}
+
+inline std::string Card::getOperator() const {
+	return operate;
 }
