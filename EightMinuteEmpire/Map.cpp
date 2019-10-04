47c8d68fcb52747ddf3fa246ff6df5898e5cbc0e
Sean Nakagawa <50758863+Deadborne@users.noreply.github.com>
2019-10-04 19:00:10 -0400

    Map Reader added

--- EightMinuteEmpire/Map.cpp
+++ EightMinuteEmpire/Map.cpp
@@ -2,19 +2,120 @@
 #include "stdafx.h"
 #include "Map.h"
 #include <iostream>
+#include <fstream>
 #include <string>
-
+#include <vector>
+#include "Country.h"
 using namespace std;
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graphviz.hpp>
+using namespace boost;
+
+
+
 int number;
+
 Map::Map(int num) {
 	number = num;
 }
+
 int Map::getNum()
 {
 	cout << "Calling from map test! \n Hit 'Enter' to close the console\n";
 	string rofl;
 	getline(cin, rofl);
 	return number;
-};
+}
+
+vector<string> Map::split(string _stringToBeSplit, string _delimeter)
+{
+	std::vector<std::string> splittedString;
+	int startIndex = 0;
+	int  endIndex = 0;
+	while ((endIndex = _stringToBeSplit.find(_delimeter, startIndex)) < _stringToBeSplit.size())
+	{
+
+		std::string val = _stringToBeSplit.substr(startIndex, endIndex - startIndex);
+		splittedString.push_back(val);
+		startIndex = endIndex + _delimeter.size();
+
+	}
+	if (startIndex < _stringToBeSplit.size())
+	{
+		std::string val = _stringToBeSplit.substr(startIndex);
+		splittedString.push_back(val);
+	}
+	return splittedString;
+
+}
+
+
+void Map::ReadMap(string f, vector <Country> cv)
+{
+	std::ifstream infile;
+	infile.open("./Maps/" + f + ".txt");
+
+	std::string str;
+
+	if (!infile) {
+		cout << "That file is not valid.\n";
+	}
+
+
+	typedef boost::adjacency_list<listS, vecS, undirectedS> Graph;
+	Graph GameMap;
+	vector<string> v;
+
+	while (std::getline(infile, str)) {
+		
+		std::string line; //the line we grabbed
+
+		line = str;
+		int countryNumber;
+		int continentNumber;
+		bool starter = false;
+		std::string allAdjacent;
+		
+		//grabs first number (country number)
+		std::size_t pos = line.find(":");
+		std::istringstream iss(line.substr(0, pos));
+		iss >> countryNumber;
+		if (line.find("S") != std::string::npos) {
+			starter = true;
+		}
+		iss.clear();
+
+		//grab the continent number
+		std::size_t pos2 = line.find("-");
+		std::istringstream iss2(line.substr(pos+1, pos2));
+		iss2 >> continentNumber;
+		iss2.clear();
+		
+		//grab adjacencies
+		std::istringstream iss3(line.substr(pos2 + 1));
+		iss3 >> allAdjacent;
+		v = split(allAdjacent, ",");
+
+		//store all adjacencies in map
+		for (int i = 0; i < v.size(); i++) {
+			
+			int box;
+			std::istringstream intvertex(v.at(i));
+			intvertex >> box;
+
+			add_edge(countryNumber, box, GameMap);
+			
+		}
+
+		//create a country, store it in the vector of countries
+		Country c = Country(countryNumber, continentNumber, starter);
+		cv.at(countryNumber) = c;
+		iss3.clear();
+	}
+
+	//output the map
+	write_graphviz(cout, GameMap);
+	infile.close();
+}
 
 
