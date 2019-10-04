031fa55d9f0837c3940a725ca6fb906404870688
Sean Nakagawa <50758863+Deadborne@users.noreply.github.com>
2019-10-04 18:59:35 -0400

    Map Reader method

--- EightMinuteEmpire/Map.h
+++ EightMinuteEmpire/Map.h
@@ -1,5 +1,11 @@
 #pragma once
 #include "stdafx.h"
+#include <string>
+#include <vector>
+#include "Country.h"
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graphviz.hpp>
+
 class Map
 {
 private:
@@ -7,4 +13,6 @@ private:
 public:
 	Map(int n);
 	int getNum();
+	std::vector<std::string> split(std::string _stringToBeSplit, std::string _delimeter);
+	void ReadMap(std::string _Filename, std::vector<Country> _v);
 };
