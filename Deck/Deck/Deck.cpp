36689c86c931e65aaae31bb28e85a2846a061175
George Mavroeidis <gdmavroeidis@hotmail.com>
2019-10-04 16:48:25 -0400

    Update Deck.cpp

--- Deck/Deck/Deck.cpp
+++ Deck/Deck/Deck.cpp
@@ -14,9 +14,13 @@ using std::vector;
 using std::shuffle;
 using std::mt19937;
 using std::random_device;
+using std::to_string;
 
+// shuffles deck randomely with different combinations in each file execution
 vector<Card> shuffleDeck(vector<Card> d);
+// draw a card from the deck and place it to the hand space
 void draw(vector<Card> d, vector<Card> h);
+// print deck of 
 void printDeck(vector<Card> d);
 
 int main()
@@ -25,57 +29,77 @@ int main()
 
 	// Deck Object
 	vector<Card> deck(42);
-	// Hand Object with face-up cards
+	// Hand Object with 6 face-up cards
 	vector<Card> hand(6);
 
-	deck[0] = Card("shard", 1, "move 2", "", "");
-	deck[1] = Card("anvil", 1, "place 3", "move 3", "or");
-	deck[2] = Card("anvil", 2, "move 4", "", "");
-	deck[3] = Card("anvil", 1, "move 4", "", "");
-	deck[4] = Card("tree", 1, "move 6", "", "");
-	deck[5] = Card("tree", 1, "move 3", "", "");
-	deck[6] = Card("anvil", 1, "place 3", "", "");
-	deck[7] = Card("shard", 1, "place 2", "", "");
-	deck[8] = Card("wildcard", 1, "moveOver 2", "", "");
-	deck[9] = Card("tree", 1, "moveOver 4", "", "");
-	deck[10] = Card("anvil", 1, "moveOver 3", "", "");
-	deck[11] = Card("wildcard", 1, "place 2", "", "");
-	deck[12] = Card("coal", 1, "place 2", "", "");
-	deck[13] = Card("shard", 1, "place 2", "", "");
-	deck[14] = Card("coal", 1, "place 3", "", "");
-	deck[15] = Card("anvil", 1, "place 3", "", "");
-	deck[16] = Card("carrot", 2, "place 3", "", "");
-	deck[17] = Card("coal", 1, "place 2", "build", "or");
-	deck[18] = Card("anvil", 1, "build", "", "");
-	deck[19] = Card("carrot", 1, "build", "", "");
-	deck[20] = Card("carrot", 1, "build", "", "");
-	deck[21] = Card("tree", 1, "build", "", "");
-	deck[22] = Card("tree", 1, "build", "", "");
-	deck[23] = Card("shard", 1, "place 2", "", "");
-	deck[24] = Card("carrot", 1, "destroy", "place 1", "and");
-	deck[24] = Card("carrot", 1, "place 4", "move 2", "or");
-	deck[25] = Card("tree", 1, "destroy", "build", "or");
-	deck[26] = Card("coal", 1, "moveOver 2", "", "");
-	deck[27] = Card("anvil", 1, "place 3", "move 4", "or");
-	deck[28] = Card("anvil", 1, "place 3", "move 4", "or");
-	deck[29] = Card("carrot", 1, "moveOver 3", "", "");
-	deck[30] = Card("tree", 1, "moveOver 3", "", "");
-	deck[31] = Card("coal", 1, "moveOver 3", "", "");
-	deck[32] = Card("carrot", 2, "move 4", "", "");
-	deck[33] = Card("advil", 1, "move 5", "", "");
-	deck[34] = Card("coal", 1, "moveOver 2", "", "");
-	deck[35] = Card("wildcard", 1, "moveOver 3", "", "");
-	deck[36] = Card("carrot", 1, "move 5", "", "");
-	deck[37] = Card("coal", 1, "move 2", "", "");
-	deck[38] = Card("tree", 1, "place 2", "move 3", "or");
-	deck[39] = Card("carrot", 1, "move 4", "", "");
-	deck[40] = Card("carrot", 1, "place 3", "", "");
-	deck[41] = Card("shard", 1, "place 1", "", "");
+	int players = 0;
+	cout << "Enter amount of Players: " << endl;
+	cin >> players;
+	while (players < 2 || players > 5) {
+		cout << "Invalid amount of players. Must be between 2 and 5" << endl;
+		cin >> players;
+	}
+	cout << "You chose " << players << " players";
 
 
+	// Fill deck with cards
+	deck[0] = Card("shard", 1, "move", 2, "", 0, "", false);
+	deck[1] = Card("anvil", 1, "place", 3, "move", 3, "or", false);
+	deck[2] = Card("anvil", 2, "move", 4, "", 0, "", false);
+	deck[3] = Card("anvil", 1, "move", 4, "", 0, "", false);
+	deck[4] = Card("tree", 1, "move", 6, "", 0, "", true);
+	deck[5] = Card("tree", 1, "move", 3, "", 0, "", false);
+	deck[6] = Card("anvil", 1, "place", 3, "", 0, "", false);
+	deck[7] = Card("shard", 1, "place", 2, "", 0, "", true);
+	deck[8] = Card("wildcard", 1, "moveOver", 2, "", 0, "", false);
+	deck[9] = Card("tree", 1, "moveOver", 4, "", 0, "", false);
+	deck[10] = Card("anvil", 1, "moveOver", 3, "", 0, "", false);
+	deck[11] = Card("wildcard", 1, "place", 2, "", 0, "", false);
+	deck[12] = Card("coal", 1, "place", 2, "", 0, "", false);
+	deck[13] = Card("shard", 1, "place", 2, "", 0, "", false);
+	deck[14] = Card("coal", 1, "place", 3, "", 0, "", false);
+	deck[15] = Card("anvil", 1, "place", 3, "", 0, "", false);
+	deck[16] = Card("carrot", 2, "place", 3, "", 0, "", false);
+	deck[17] = Card("coal", 1, "place", 2, "build", 1, "or", false);
+	deck[18] = Card("anvil", 1, "build", 1, "", 0, "", false);
+	deck[19] = Card("carrot", 1, "build", 1, "", 0, "", false);
+	deck[20] = Card("carrot", 1, "build", 1, "", 0, "", false);
+	deck[21] = Card("tree", 1, "build", 1, "", 0, "", false);
+	deck[22] = Card("tree", 1, "build", 1, "", 0, "", false);
+	deck[23] = Card("shard", 1, "place", 2, "", 0, "", false);
+	deck[24] = Card("carrot", 1, "destroy", 1, "place", 1, "and", false);
+	deck[24] = Card("carrot", 1, "place", 4, "move", 2, "or", true);
+	deck[25] = Card("tree", 1, "destroy", 1, "build", 1, "or", false);
+	deck[26] = Card("coal", 1, "moveOver", 2, "", 0, "", false);
+	deck[27] = Card("anvil", 1, "place", 3, "move", 4, "or", false);
+	deck[28] = Card("anvil", 1, "place", 3, "move", 4, "or", false);
+	deck[29] = Card("carrot", 1, "moveOver", 3, "", 0, "", false);
+	deck[30] = Card("tree", 1, "moveOver", 3, "", 0, "", false);
+	deck[31] = Card("coal", 1, "moveOver", 3, "", 0, "", false);
+	deck[32] = Card("carrot", 2, "move", 4, "", 0, "", false);
+	deck[33] = Card("advil", 1, "move", 5, "", 0, "", false);
+	deck[34] = Card("coal", 1, "moveOver", 2, "", 0, "", false);
+	deck[35] = Card("wildcard", 1, "moveOver", 3, "", 0, "", false);
+	deck[36] = Card("carrot", 1, "move", 5, "", 0, "", false);
+	deck[37] = Card("coal", 1, "move", 2, "", 0, "", false);
+	deck[38] = Card("tree", 1, "place", 2, "move", 3, "or", false);
+	deck[39] = Card("carrot", 1, "move", 4, "", 0, "", false);
+	deck[40] = Card("carrot", 1, "place", 3, "", 0, "", false);
+	deck[41] = Card("shard", 1, "place", 1, "", 0, "", false);
 
-	// shuffles deck before game
+	cout << "\n" << endl;
+
+	// Move 6 front cards from Deck to Hand object
+	for (int i = 0; i < hand.size(); i++) {
+		hand[i] = deck[0];
+		deck.erase(deck.begin());
+		deck.size();
+	}
+
+	// Print shuffled deck
 	printDeck(shuffleDeck(deck));
+	// Print hand for display 
+	printDeck(hand);
 
 	return 0;
 
@@ -97,14 +121,19 @@ vector<Card> shuffleDeck(vector<Card> d) {
 
 // Print cards from deck based on their good
 void printDeck(vector<Card> d) {
-	for (auto& i : d)
-		cout << "Goods: " << i.getGoodAmount() << " " << i.getGood() << ", Action1: " << i.getAction1() << ", Action2: " << i.getAction2() << " " << endl;
+	int j = 0;
+	string str;
+	for (auto& i : d) {
+		str = "[" + to_string(j) + "] " + "Goods" + to_string(i.getGoodAmount()) + " " + i.getGood() + ", Action 1: " + i.getAction1() + "x" + to_string(i.getNumA1()) + ", Action 2: " + i.getAction2() + "x" + to_string(i.getNumA2()) + " 5-Player-Card: " + to_string(i.getIsFive());
+		cout << str << endl;
+		j++;
+	}
 }
 
 void printCard(vector<Card> d, int i) {
-	cout << "Goods: " << d[i].getGoodAmount() << " " << d[i].getGood() << ", Action1: " << d[i].getAction1() << ", Action2: " << d[i].getAction2() << " " <<  endl;
+	string str = "[" + to_string(i) + "] " + "Goods" + to_string(d[i].getGoodAmount()) + " " + d[i].getGood() + ", Action 1: " + d[i].getAction1() + "x" + to_string(d[i].getNumA1()) + ", Action 2: " + d[i].getAction2() + "x" + to_string(d[i].getNumA2()) + " 5-Player-Card: " + to_string(d[i].getIsFive());
+	cout << str << endl;
 }
-
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
