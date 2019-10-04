0d31a252fdc07f3f63240a8074daa1efc00ae41c
George Mavroeidis <gdmavroeidis@hotmail.com>
2019-10-04 16:35:28 -0400

    Update Card.h

--- Deck/Deck/Card.h
+++ Deck/Deck/Card.h
@@ -6,17 +6,23 @@ class Card {
 		std::string good;
 		int amount;
 		std::string action1;
+		int num1;
 		std::string action2;
+		int num2;
 		std::string operate;
+		bool isFive;
 
 	public:
 		Card();
-		Card(std::string gd, int num, std::string act1, std::string act2, std::string op);
+		Card(std::string gd, int num, std::string act1, int n1, std::string act2, int n2, std::string op, bool n5);
 		Card(const Card &c);
 		~Card();
 		std::string getGood() const;
 		int getGoodAmount() const;
 		std::string getAction1() const;
+		int getNumA1() const;
 		std::string getAction2() const;
+		int getNumA2() const;
 		std::string getOperator() const;
+		bool getIsFive() const;
 };
